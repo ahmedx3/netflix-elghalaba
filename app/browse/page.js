af3d3fa5a87c50015e7dc07a20e2fc48a6d07c73
ahmedmostafa-incorta <ahmed.moustafaa99@gmail.com>
2023-06-17 22:56:39 +0300

    Added movie card component

--- app/browse/page.js
+++ app/browse/page.js
@@ -1,6 +1,7 @@
 'use client';
 
 import { useRouter } from 'next/navigation';
+import MovieCard from '@/components/MovieCard';
 
 export default function browse() {
   const router = useRouter();
@@ -58,13 +59,39 @@ export default function browse() {
                 </button>
                 <button
                   type='button'
-                  class='text-white hover:text-white border border-red-700 hover:bg-red-800  focus:outline-none  font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-red-500 dark:text-white dark:hover:text-white dark:hover:bg-red-600 '
+                  className='inline-flex text-white hover:text-white border border-red-700 hover:bg-red-800  focus:outline-none  font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-red-500 dark:text-white dark:hover:text-white dark:hover:bg-red-600 '
                 >
-                  Add to Watchlist
+                  <svg
+                    fill='none'
+                    stroke='currentColor'
+                    stroke-width='1.5'
+                    viewBox='0 0 24 24'
+                    xmlns='http://www.w3.org/2000/svg'
+                    aria-hidden='true'
+                    className='w-5 h-5 mr-2 -ml-1'
+                  >
+                    <path
+                      stroke-linecap='round'
+                      stroke-linejoin='round'
+                      d='M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z'
+                    ></path>
+                  </svg>
+                  Add Watchlist
                 </button>
               </div>
             </div>
           </div>
+        </div>
+      </div>
+      <div className='container'>
+        <h1 className='text-2xl my-4 font-bold'>Just Released</h1>
+        {/* <div className='flex flex-row space-x-4 my-2'> */}
+        <div className='grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-4'>
+          <MovieCard />
+          <MovieCard />
+          <MovieCard />
+          <MovieCard />
+          <MovieCard />
         </div>
       </div>
     </div>
---
+++ components/MovieCard.js
@@ -0,0 +1,54 @@
+function movieCard() {
+  return (
+    <div class='relative'>
+      <img
+        src='https://image.tmdb.org/t/p/w500/fiVW06jE7z9YnO4trhaMEdclSiC.jpg'
+        className='rounded-lg opacity-75 object-cover'
+      />
+      <h3 class='absolute top-5 right-5'>
+        <svg
+          fill='none'
+          stroke='currentColor'
+          stroke-width='2.5'
+          viewBox='0 0 24 24'
+          xmlns='http://www.w3.org/2000/svg'
+          aria-hidden='true'
+          className='w-5 h-5 mr-2 -ml-1 hover:cursor-pointer'
+        >
+          <path
+            stroke-linecap='round'
+            stroke-linejoin='round'
+            d='M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z'
+          ></path>
+        </svg>
+      </h3>
+      <h3 class='absolute text-lg bottom-12 left-5 font-bold'>Fast X</h3>
+      <h3 class='absolute bottom-5 left-5'>
+        <div class='flex items-center '>
+          <svg
+            aria-hidden='true'
+            class='w-5 h-5 text-yellow-400'
+            fill='currentColor'
+            viewBox='0 0 20 20'
+            xmlns='http://www.w3.org/2000/svg'
+          >
+            <title>Rating star</title>
+            <path d='M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z'></path>
+          </svg>
+          <p class='ml-2 text-sm font-bold text-gray-900 dark:text-white'>
+            4.95
+          </p>
+          <span class='w-1 h-1 mx-1.5 bg-gray-500 rounded-full dark:bg-gray-400'></span>
+          <p
+            href='#'
+            class='text-sm font-medium text-gray-900  dark:text-white'
+          >
+            | Action . Movie
+          </p>
+        </div>
+      </h3>
+    </div>
+  );
+}
+
+export default movieCard;
